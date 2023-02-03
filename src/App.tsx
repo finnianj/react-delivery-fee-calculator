13942ceb09eb721bff57589631f7798e34e241b2
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-02-03 14:49:06 +0100

    some css

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,28 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-      Finn's first React/Typescript App
+      <h1 id="title">
+        Delivery Fee Calculator
+      </h1>
+      <div className="row">
+        <h2>Cart Value</h2> <input placeholder="Enter value..."></input>
+      </div>
+      <div className="row">
+        <h2>Delivery Distance</h2> <input placeholder="Enter value..."></input>
+      </div>
+      <div className="row">
+        <h2>Amount of items</h2> <input placeholder="Enter value..."></input>
+      </div>
+      <div className="row">
+        <h2>Date & Time</h2><input type="datetime-local"></input>
+      </div>
+        <h2>
+          <button id="calculate_button">Calculate Delivery Price</button>
+        </h2>
+      <div className="row">
+        <h1>Total: </h1>
+        <h1 id="total">€ 5</h1>
+      </div>
     </div>
   );
 }
