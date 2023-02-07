acdf1c11cec52610b1fa32fee9d22565a4b98aa6
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-02-07 12:04:31 +0100

    info

--- src/Info.tsx
+++ src/Info.tsx
@@ -13,18 +13,7 @@ function Info() {
       <p>
         But although I'm a rookie, I have never been so excited about learning. It's been a pretty life-changing process actually. I am dicovering more everyday and having a great time as I go. I especially love functional programming and trying to write elegant logic with Javascript! 🪄
       </p>
-      <p>
-        I would love to meet for an interview if you think I might be a good fit for the internship. However, I'm sure there will be some great programmers applying, and to be honest, I would be very grateful just to get some feedback on my code. 🙇‍♂️
-      </p>
       <h2>Thank you 😊</h2>
-
-      <p>
-        <em>P.S: I used to work for Wolt back in 2020 - Greetings to Marvin and Chris!</em>
-      </p>
-      <div id="image_container">
-        <img src="https://avatars.githubusercontent.com/u/114495682?s=400&u=5783c5b3cf5432e8118558cbd87292459dd232dc&v=4" id="me" alt="a silly bandana"></img>
-        <a href="https://www.linkedin.com/in/finnianj/" target="_blank" rel="noreferrer">LinkedIn</a>
-      </div>
     </div>
   );
 }
